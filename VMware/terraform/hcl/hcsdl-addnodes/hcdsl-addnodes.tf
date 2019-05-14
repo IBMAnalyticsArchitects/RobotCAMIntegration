46c03ac4e19e24b962d4a95f867e818f9b8f5c17
jlerm <jlerm@us.ibm.com>
2019-05-14 00:13:50 -0500

    Updates

--- VMware/terraform/hcl/hcsdl-addnodes/hcdsl-addnodes.tf
+++ VMware/terraform/hcl/hcsdl-addnodes/hcdsl-addnodes.tf
@@ -399,7 +399,7 @@ chmod 700 runRemote.sh
 scp runRemote.sh ${var.driver_ip}:/opt/cloud_install_${var.node_label}/
 
 # Invoke runRemote.sh w/ nohup
-ssh ${var.driver_ip} "set -x; nohup /opt/cloud_install_${var.node_label}/runRemote.sh &"
+ssh ${var.driver_ip} "set -x; cd /opt/cloud_install_${var.node_label}/; nohup /opt/cloud_install_${var.node_label}/runRemote.sh &"
 
 EOF
 
