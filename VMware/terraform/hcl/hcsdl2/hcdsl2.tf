940ab15e88cf607fd669a8e10f8d4d3a9f530bdc
jlerm <jlerm@us.ibm.com>
2019-05-24 21:58:19 -0500

    Testing provider alias in driver resource for hcsdl/vmware

--- VMware/terraform/hcl/hcsdl2/hcdsl2.tf
+++ VMware/terraform/hcl/hcsdl2/hcdsl2.tf
@@ -230,7 +230,7 @@ locals {
 
 # Driver 
 resource "vsphere_virtual_machine" "driver" {
-  provider = ${var.provider_alias}
+  provider = "${var.provider_alias}"
   name = "${var.vm_name_prefix}-drv"
   num_cpus = "4"
   memory = "4096"
