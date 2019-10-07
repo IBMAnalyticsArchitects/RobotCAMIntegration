bd74f6d7f39667b03b3273a8c2f48bfcc1606974
jlerm <jlerm@us.ibm.com>
2019-10-07 16:09:02 -0500

    Added VMware template for a set of simple dev VMs

---
+++ VMware/terraform/hcl/dev-vms-vmware/dev-vms.tf
@@ -0,0 +1,303 @@
+
+##############################################################
+# Keys - CAMC (public/private) & optional User Key (public)
+##############################################################
+variable "allow_unverified_ssl" {
+  description = "Communication with vsphere server with self signed certificate"
+  default = "true"
+}
+
+##############################################################
+# Define the vsphere provider
+##############################################################
+
+provider "vsphere" {
+  allow_unverified_ssl = "${var.allow_unverified_ssl}"
+  version = "~> 1.3" 
+}
+
+##############################################################
+# Define pattern variables
+##############################################################
+
+
+##############################################################
+# Vsphere data for provider
+##############################################################
+data "vsphere_datacenter" "vm_datacenter" {
+  name = "${var.vm_datacenter}"
+}
+data "vsphere_datastore" "vm_datastore" {
+  name = "${var.vm_root_disk_datastore}"
+  datacenter_id = "${data.vsphere_datacenter.vm_datacenter.id}"
+}
+data "vsphere_resource_pool" "vm_resource_pool" {
+  name = "${var.vm_resource_pool}"
+  datacenter_id = "${data.vsphere_datacenter.vm_datacenter.id}"
+}
+data "vsphere_network" "vm_network" {
+  name = "${var.vm_network_interface_label}"
+  datacenter_id = "${data.vsphere_datacenter.vm_datacenter.id}"
+}
+
+data "vsphere_virtual_machine" "vm_template" {
+  name = "${var.vm-image}"
+  datacenter_id = "${data.vsphere_datacenter.vm_datacenter.id}"
+}
+
+##### Image Parameters variables #####
+
+variable "vm_name_prefix" {
+  description = "Prefix for vm names"
+}
+
+
+#########################################################
+##### Resource : vm
+#########################################################
+variable "ssh_user" {
+  description = "The user for ssh connection, which is default in template"
+  default     = "root"
+}
+
+variable "ssh_user_password" {
+  description = "The user password for ssh connection, which is default in template"
+}
+
+
+variable "num_client_vms" {
+  description = "Number of client VMs to create"
+}
+
+variable "vm_datacenter" {
+  description = "Target vSphere datacenter for virtual machine creation"
+}
+
+variable "vm_domain" {
+  description = "Domain Name of virtual machine"
+}
+
+variable "vm_number_of_vcpu" {
+  description = "Number of virtual CPU for the virtual machine, which is required to be a positive Integer"
+  default = "1"
+}
+
+variable "vm_memory" {
+  description = "Memory assigned to the virtual machine in megabytes. This value is required to be an increment of 1024"
+  default = "1024"
+}
+
+
+variable "vm_resource_pool" {
+  description = "Target vSphere Resource Pool to host the virtual machine"
+}
+
+variable "vm_dns_servers" {
+  type = "list"
+  description = "DNS servers for the virtual network adapter"
+}
+
+
+variable "time_server" {
+  description = "Hostname or IPv4 for time server"
+}
+
+variable "vm_network_interface_label" {
+  description = "vSphere port group or network label for virtual machine's vNIC"
+}
+
+variable "vm_ipv4_gateway" {
+  description = "IPv4 gateway for vNIC configuration"
+}
+
+variable "vm_start_ipv4_address" {
+  description = "Start IPv4 address for vNIC configuration"
+}
+
+variable "vm_ipv4_prefix_length" {
+  description = "IPv4 prefix length for vNIC configuration. The value must be a number between 8 and 32"
+}
+
+variable "vm_adapter_type" {
+  description = "Network adapter type for vNIC Configuration"
+  default = "vmxnet3"
+}
+
+variable "vm_root_disk_datastore" {
+  description = "Data store or storage cluster name for target virtual machine's disks"
+}
+
+variable "vm_root_disk_type" {
+  type = "string"
+  description = "Type of template disk volume"
+  default = "thin"
+}
+
+variable "vm_root_disk_controller_type" {
+  type = "string"
+  description = "Type of template disk controller"
+  default = "scsi"
+}
+
+variable "vm_root_disk_size" {
+  description = "Size of template disk volume. Should be equal to template's disk size"
+  default = "25"
+}
+
+variable "vm_data_disk_size" {
+  description = "Client VM Data Disk Size"
+  default = "100"
+}
+
+variable "vm-image" {
+  description = "Operating system image id / template that should be used when creating the virtual image"
+}
+
+variable "public_nic_name" {
+  description = "Name of the public network interface"
+  default = "ens192"
+}
+
+variable "public_ssh_key" {
+  description = "Public SSH Key"
+}
+
+
+########
+# Isolate IP address components:
+locals {
+  vm_ipv4_address_elements = "${split(".",var.vm_start_ipv4_address)}"
+  vm_ipv4_address_base = "${format("%s.%s.%s",local.vm_ipv4_address_elements[0],local.vm_ipv4_address_elements[1],local.vm_ipv4_address_elements[2])}"
+  vm_ipv4_address_start= "${local.vm_ipv4_address_elements[3] }"
+}
+
+###########################################################################################################################################################
+
+
+
+
+############################################################################################################################################################
+
+# Dev VMs
+resource "vsphere_virtual_machine" "devvm" {
+	count  = "${var.num_client_vms}"
+  name = "${var.vm_name_prefix}-${ count.index }"
+  num_cpus = "${var.vm_number_of_vcpu}"
+  memory = "${var.vm_memory}"
+  resource_pool_id = "${data.vsphere_resource_pool.vm_resource_pool.id}"
+  datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+  guest_id = "${data.vsphere_virtual_machine.vm_template.guest_id}"
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.vm_template.id}"
+    customize {
+      linux_options {
+        domain = "${var.vm_domain}"
+        host_name = "${var.vm_name_prefix}-${ count.index }"
+      }
+      network_interface {
+        ipv4_address = "${local.vm_ipv4_address_base }.${local.vm_ipv4_address_start }"
+        ipv4_netmask = "${ var.vm_ipv4_prefix_length }"
+      }
+    ipv4_gateway = "${var.vm_ipv4_gateway}"
+    }
+  }
+
+  network_interface {
+    network_id = "${data.vsphere_network.vm_network.id}"
+    adapter_type = "${var.vm_adapter_type}"
+  }
+
+  disk {
+    label = "${var.vm_name_prefix}0.vmdk"
+    size = "${var.vm_root_disk_size}"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+  }
+  
+  disk {
+    label = "${var.vm_name_prefix}1.vmdk"
+    size = "1000"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+    unit_number = "1"
+  }
+  
+  disk {
+    label = "${var.vm_name_prefix}2.vmdk"
+    size = "${var.vm_data_disk_size}"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+    unit_number = "2"
+  }
+  
+  disk {
+    label = "${var.vm_name_prefix}3.vmdk"
+    size = "${var.vm_data_disk_size}"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+    unit_number = "3"
+  }
+  
+  disk {
+    label = "${var.vm_name_prefix}4.vmdk"
+    size = "${var.vm_data_disk_size}"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+    unit_number = "4"
+  }
+  
+  disk {
+    label = "${var.vm_name_prefix}5.vmdk"
+    size = "${var.vm_data_disk_size}"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+    unit_number = "5"
+  }
+  
+  disk {
+    label = "${var.vm_name_prefix}6.vmdk"
+    size = "${var.vm_data_disk_size}"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+    unit_number = "6"
+  }
+  
+  disk {
+    label = "${var.vm_name_prefix}7.vmdk"
+    size = "${var.vm_data_disk_size}"
+    keep_on_remove = "false"
+    datastore_id = "${data.vsphere_datastore.vm_datastore.id}"
+    unit_number = "7"
+  }
+  
+  connection {
+    type = "ssh"
+    user     = "${var.ssh_user}"
+    password = "${var.ssh_user_password}"
+    host     = "${self.clone.0.customize.0.network_interface.0.ipv4_address}"
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /root/.ssh",
+      "chmod 700 /root/.ssh",
+      "echo ${var.public_ssh_key} > /root/.ssh/authorized_keys",
+      "chmod 600 /root/.ssh/authorized_keys",
+      "echo StrictHostKeyChecking no > /root/.ssh/config",
+      "chmod 600 /root/.ssh/config",
+      "systemctl disable NetworkManager",
+      "systemctl stop NetworkManager",
+      "echo nameserver ${var.vm_dns_servers[0]} > /etc/resolv.conf",
+      "pvcreate /dev/sdb",
+      "vgextend vg_node1 /dev/sdb",
+      "lvextend /dev/vg_node1/lv_root /dev/sdb",
+      "resize2fs /dev/mapper/vg_node1-lv_root"
+    ]
+ }
+
+
+}
+
+
+
