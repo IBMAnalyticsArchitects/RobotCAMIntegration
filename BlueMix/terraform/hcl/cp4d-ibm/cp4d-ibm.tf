a07628612a2e12f57a13fd8ee9f8e3b3da17cebc
jlerm <jlerm@us.ibm.com>
2020-02-13 10:24:34 -0600

    Added property to enable/disable public interface on cp4d haproxy on ibm cloud

--- BlueMix/terraform/hcl/cp4d-ibm/cp4d-ibm.tf
+++ BlueMix/terraform/hcl/cp4d-ibm/cp4d-ibm.tf
@@ -204,6 +204,10 @@ variable "install_portworx" {
   description = "Install Portworx (0/1)"
 }
 
+variable "private_load_balancer" {
+  description = "Private Load Balancer (true/false)"
+}
+
 locals {
   idm_install = "${ var.idm_primary_hostname=="" || var.idm_primary_ip=="" || var.idm_admin_password=="" || var.idm_ldapsearch_password=="" || var.idm_directory_manager_password=="" ? 1 : 0 }"
 }
@@ -477,7 +481,7 @@ resource "ibm_compute_vm_instance" "icphaproxy" {
   private_vlan_id          = "${data.ibm_network_vlan.cluster_vlan.id}"
   network_speed            = 1000
   hourly_billing           = true
-  private_network_only     = true
+  private_network_only     = ${var.private_load_balancer}
   cores                    = 4
   memory                   = 4096
   disks                    = [100,1000]
