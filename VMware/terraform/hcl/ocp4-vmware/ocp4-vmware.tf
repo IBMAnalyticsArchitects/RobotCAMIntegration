7b04ba80fc98286dd183fd76d700aa0027e06f97
jlerm <jlerm@us.ibm.com>
2020-05-20 16:07:19 -0500

    test

--- VMware/terraform/hcl/ocp4-vmware/ocp4-vmware.tf
+++ VMware/terraform/hcl/ocp4-vmware/ocp4-vmware.tf
@@ -223,7 +223,7 @@ locals {
   num_worker = "${var.num_workers}"
   
   
-  master_hostnames = for i in range(num_master) : format("master-%s", i)
+  master_hostnames = for i in range(local.num_master) : format("master-%s", i)
 
 
 }
